bc19bc53794ff3ee0b55da83df84f6dd5254793a
Saumya Singh <singhsaumyait034@gmail.com>
2024-09-04 12:52:02 +0530

    Serialization

---
+++ src/main/java/SerializationPOJO/POJO1.java
@@ -0,0 +1,31 @@
+package SerializationPOJO;
+
+public class POJO1 {
+
+        /*
+    Serialization Converting Java Object to JSON(Request Payload)
+     */
+
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDisplay() {
+        return display;
+    }
+
+    public void setDisplay(String display) {
+        this.display = display;
+    }
+
+    private String display;
+
+
+
+    public String getMessage(){
+        return message;
+    }
+
+}
---
+++ src/test/java/SerializationConcept/ConvertingJavaObject.java
@@ -0,0 +1,24 @@
+package SerializationConcept;
+
+import SerializationPOJO.POJO1;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class ConvertingJavaObject {
+
+    POJO1 pojo = new POJO1();
+    @Test
+    public void serializationConcept(){
+        pojo.setMessage("Hello This is Saumya Messgae");
+        pojo.setDisplay("Hello Saumya Display");
+
+        given().log().all()
+                .body(pojo)
+                .when()
+                .post("/")
+                .then()
+                .statusCode(200);
+
+    }
+}
