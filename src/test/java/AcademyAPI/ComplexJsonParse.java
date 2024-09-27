00957b5d3ee41f29644ba3e35ce17f0839adee38
Saumya Singh <singhsaumyait034@gmail.com>
2024-09-27 22:25:33 +0530

    Iteration to Json is done

--- src/test/java/AcademyAPI/ComplexJsonParse.java
+++ src/test/java/AcademyAPI/ComplexJsonParse.java
@@ -7,21 +7,6 @@ public class ComplexJsonParse {
     public static void main(String[] args){
         JsonPath jsRahul = new JsonPath(PostRequestPayload.complexJsonRahul());
 
-
-        /*
-
-
-
-
-        3.
-
-
-
-        5. Print no of copies sold by RPA Course
-
-        6. Verify if Sum of all Course prices matches with Purchase Amount
-         */
-
         // 1. Print No of courses returned by API
         int size =  jsRahul.get("courses.size()");
         System.out.println(size);
@@ -33,11 +18,28 @@ public class ComplexJsonParse {
         System.out.println(jsRahul.getString("courses[0].title"));
 
         //4. Print All course titles and their respective Prices
-        for(int i=0;i<size;i++){
-            String title = jsRahul.get("courses["+i+"].title");
-            int prices = jsRahul.getInt("courses["+i+"].price");
-           System.out.println(title+" ---> " +prices);
+        for(int i=0;i<size;i++) {
+            String title = jsRahul.get("courses[" + i + "].title");
+            int prices = jsRahul.getInt("courses[" + i + "].price");
+            System.out.println(title + " ---> " + prices);
         }
+           //5.. Print no of copies sold by RPA Course
+            for(int i=0;i<size;i++){
+                if(jsRahul.getString("courses["+i+"].title").equalsIgnoreCase("RPA")){
+                    System.out.println(jsRahul.getInt("courses["+i+"].copies"));
+                }
+            }
+            //6.Verify if Sum of all Course prices matches with Purchase Amount
+        int sum=0;
+            int purchaseAmount = jsRahul.get("dashboard.purchaseAmount");
+            for(int i=0;i<size;i++){
+                sum += jsRahul.getInt("courses["+i+"].price");
+            }
+            if(sum==purchaseAmount)
+                System.out.println("Both Amounts are same");
+            else
+                System.out.println("Amounts are different");
+
 
 
 
